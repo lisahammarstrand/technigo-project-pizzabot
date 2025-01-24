bdb48c0a6b4444426c76b114b4895bbf708245d9
Your Name <you@example.com>
2025-01-24 16:12:11 +0100

    Start pizza bot prompting

--- code/script.js
+++ code/script.js
@@ -6,8 +6,24 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+let person = prompt(`Please enter your name`, `Name`)
+
+if (person !== null) {
+  alert(`Hi and welcome ${person}! How are you today?`)
+}
+
 // Step 2 - Food choice
 // Your code goes here
+
+let number = prompt(`Please select your order: 1 = Pizza, 2 = Pasta, 3 = Salad`, `Enter number`)
+
+if (number === "1") {
+  alert(`You have chosen pizza, good choise!`)
+} else if (number === "2") {
+  alert(`You have chosen pasta, good choise!`)
+} else {
+  alert(`You have chosen salad, good choise!`)
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
